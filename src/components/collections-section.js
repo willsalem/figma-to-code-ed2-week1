4c296369ae347705322d758f9f94a03568eb2763
willsalem <fwillsalem@gmail.com>
2024-08-02 17:40:24 +0200

    final build

--- src/components/collections-section.js
+++ src/components/collections-section.js
@@ -17,7 +17,6 @@ const CollectionsSection = () => {
 
   const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   return (
     <div className="collections-section">
